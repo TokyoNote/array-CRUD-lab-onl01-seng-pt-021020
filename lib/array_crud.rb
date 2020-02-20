12725b9fb104d912aa5541f990418d956f36569e
Camiyah Goins <tokyonote9@gmail.com>
2020-02-20 16:59:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   ["Kozi", "Kori", "Kyli"]
-  add_element_to_end_of_array(array, element) << "Kloi"
+  add_element_to_end_of_array << "Kloi"
 end
 
 def add_element_to_start_of_array(array, element)
